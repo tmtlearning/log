5d8a38fb6c057e9357d79ad1f45a90eab320abb6
anonym <mail@example.com>
2013-02-22 10:27:29 +0400

    2nd commit

--- log.py
+++ log.py
@@ -2,6 +2,7 @@ import math
 
 INPUT = 'input.txt'
 
+
 def countLog(arr):
     a1p = []
     a10 = []
@@ -15,15 +16,18 @@ def countLog(arr):
         a1p.append(math.log1p(number))
         a10.append(math.log10(number))
         for x in range(1, len(a1p)):
-        	#print ('%s %s' % ('ln','lg'))
-        	print ('%4f %4f' % (a1p[x], a10[x]))
+            #print ('%s %s' % ('ln','lg'))
+            print ('%4f %4f' % (a1p[x], a10[x]))
 
 
-try:
-	with open(INPUT, 'r') as file_in:
-		data = file_in.readlines()
-		file_in.close()
-		countLog(data)
-
-except FileNotFoundError:
-    print('Oops! File not found!')
+while input('Do you want to exit? ') != 'yes':
+    zzz = input('Enter filename: ')
+    print(zzz)
+    try:
+        with open(str(zzz), 'r') as file_in:
+            data = file_in.readlines()
+            file_in.close()
+            countLog(data)
+    except FileNotFoundError:
+        print('Oops! File not found! Try again\n')
+input('Bye!')
